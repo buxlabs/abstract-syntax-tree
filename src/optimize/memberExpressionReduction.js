70026c26c0594eed2f85af8f29b2904cce989efe
Emil Ajdyna <emil@ajdyna.com>
2020-06-15 13:42:40 +0200

    2.8.0

--- src/optimize/memberExpressionReduction.js
+++ src/optimize/memberExpressionReduction.js
@@ -1,8 +1,8 @@
 module.exports = function memberExpressionReduction (node) {
   if (
-    node.type === "MemberExpression" &&
-    node.object.type === "ObjectExpression" &&
-    node.property.type === "Identifier"
+    node.type === 'MemberExpression' &&
+    node.object.type === 'ObjectExpression' &&
+    node.property.type === 'Identifier'
   ) {
     const property = node.object.properties.find(property => property.key.type === 'Identifier' && property.key.name === node.property.name)
     if (property && property.value.type === 'Literal') {
