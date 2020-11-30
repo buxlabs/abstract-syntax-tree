3217a3f2f659eab92abaf034c96c1a1088e398ce
Emil Ajdyna <emil@ajdyna.com>
2020-11-30 19:54:33 +0100

    Build a bundle with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,28 @@
-import { terser } from 'rollup-plugin-terser';
+import { terser } from 'rollup-plugin-terser'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
+import json from '@rollup/plugin-json'
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 export default {
-    input: 'index.js',
-    output: [
-        {
-            file: 'dist/index.js',
-            format: 'umd'
-        },
-        {
-            file: 'dist/index.min.js',
-            format: 'umd',
-            plugins: [terser()]
-        }
-    ],
-};
+  input: 'index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'umd',
+      name: 'AbstractSyntaxTree'
+    },
+    {
+      file: 'dist/index.min.js',
+      format: 'umd',
+      plugins: [terser()],
+      name: 'AbstractSyntaxTree'
+    }
+  ],
+  plugins: [
+    nodePolyfills(),
+    nodeResolve(),
+    commonjs(),
+    json()
+  ]
+}
