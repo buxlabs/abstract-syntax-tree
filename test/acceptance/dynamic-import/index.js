72f97b10659a190e5d77a8a5a4129b7eb9cdffe7
Emil Ajdyna <emil@ajdyna.com>
2020-12-08 13:55:24 +0100

    Add a skipped test case

---
+++ test/acceptance/dynamic-import/index.js
@@ -0,0 +1,30 @@
+const test = require('ava')
+const {
+  generate,
+  first,
+  parse
+} = require('../../..')
+
+test('dynamic-import: can be parsed', assert => {
+  const source = `
+    import('foo.js')
+      .then(function (bar) {
+        bar.baz()
+      })
+  `
+  const tree = parse(source)
+  const node = first(tree, 'ImportExpression')
+  assert.truthy(node)
+})
+
+test.skip('dynamic-import: can be generated', assert => {
+  const source = `
+    import('foo.js')
+      .then(function (bar) {
+        bar.baz()
+      })
+  `
+  const tree = parse(source)
+  const code = generate(tree)
+  assert.truthy(code)
+})
