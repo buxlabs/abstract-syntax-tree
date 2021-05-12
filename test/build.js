568418926c9ce41e85afaa9ed57b1f8c670d297c
Emil Ajdyna <emil@ajdyna.com>
2021-05-12 09:10:47 +0200

    Add a failing test case

--- test/build.js
+++ test/build.js
@@ -4,7 +4,7 @@ const { join } = require('path')
 const rollup = require('rollup')
 const config = require('../rollup.config')
 
-test('build: creates a valid bundle', async assert => {
+test.skip('build: creates a valid bundle', async assert => {
   const bundle = await rollup.rollup(config)
   const file = join(tmpdir(), 'bundle.js')
   await bundle.write({
@@ -13,5 +13,6 @@ test('build: creates a valid bundle', async assert => {
   })
   const AbstractSyntaxTree = require(file)
   const tree = new AbstractSyntaxTree('const foo = 42')
-  assert.truthy(tree)
+  const literal = tree.find('Literal')
+  assert.truthy(literal)
 })
