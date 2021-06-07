fb93667f50cf9e458d83386f33c4f706af92a413
Emil Ajdyna <emil@ajdyna.com>
2021-06-07 08:56:57 +0200

    Add a spec to ensure that async await works

---
+++ test/acceptance/async-await/index.js
@@ -0,0 +1,21 @@
+const test = require('ava')
+const {
+  generate,
+  first,
+  parse
+} = require('../../..')
+
+test('async-await: can be parsed and generated', assert => {
+  const source = `
+    async function foo () {}
+
+    (async () => {
+      await foo()
+    })
+  `
+  const tree = parse(source)
+  assert.truthy(first(tree, 'AwaitExpression'))
+  assert.truthy(first(tree, 'FunctionDeclaration[async=true]'))
+  const output = generate(tree)
+  assert.truthy(output)
+})
