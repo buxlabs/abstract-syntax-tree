0b43949ccda790361b244781d794460f17236fc3
Emil Ajdyna <emil@ajdyna.com>
2021-06-09 22:23:57 +0200

    Don't use esquery for simple selectors

--- src/find.js
+++ src/find.js
@@ -1,8 +1,12 @@
 const esquery = require('esquery')
 const traverse = require('./traverse')
 const equal = require('./equal')
+const TYPES = require('../types.json')
 
 module.exports = function find (tree, selector) {
+  if (TYPES.includes(selector)) {
+    selector = { type: selector }
+  }
   if (typeof selector === 'string') {
     return esquery(tree, selector)
   }
---
+++ test/find/type.js
@@ -0,0 +1,20 @@
+const test = require('ava')
+const { parse, find } = require('../..')
+
+test('find: by type (Program)', async assert => {
+  const tree = parse('const foo = \'bar\'')
+  const [node] = find(tree, 'Program')
+  assert.deepEqual(node.type, 'Program')
+})
+
+test('find: by type (Identifier)', async assert => {
+  const tree = parse('const foo = \'bar\'')
+  const [node] = find(tree, 'Identifier')
+  assert.deepEqual(node.name, 'foo')
+})
+
+test('find: by type (Literal)', async assert => {
+  const tree = parse('const foo = \'bar\'')
+  const [node] = find(tree, 'Literal')
+  assert.deepEqual(node.value, 'bar')
+})
